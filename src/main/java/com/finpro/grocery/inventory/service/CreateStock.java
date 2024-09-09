2dcfe24f958e08e23120d2063044cad87c909bea
rinaldes duma <rinaldes.duma@gmail.com>
2024-09-09 19:45:49 +0700

    feat: :sparkles: Inventory auto create when product created & new feature to create missing stock

--- src/main/java/com/finpro/grocery/inventory/service/CreateStock.java
+++ src/main/java/com/finpro/grocery/inventory/service/CreateStock.java
@@ -1,5 +1,7 @@
 package com.finpro.grocery.inventory.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +10,13 @@ import com.finpro.grocery.inventory.dto.response.ResponseInventoryDTO;
 import com.finpro.grocery.inventory.entity.Inventory;
 import com.finpro.grocery.inventory.repository.InventoryRepository;
 import com.finpro.grocery.product.entity.Product;
+import com.finpro.grocery.product.repository.ProductRepository;
 import com.finpro.grocery.product.service.ReadProduct;
 import com.finpro.grocery.share.exception.BadRequestException;
 import com.finpro.grocery.share.exception.ResourceNotFoundException;
 import com.finpro.grocery.share.sequence.service.SequenceService;
 import com.finpro.grocery.store.entity.Store;
+import com.finpro.grocery.store.repository.StoreRepository;
 import com.finpro.grocery.store.service.StoreService;
 
 import jakarta.transaction.Transactional;
@@ -24,6 +28,12 @@ public class CreateStock {
 
   @Autowired
   private ReadProduct productService;
+
+  @Autowired
+  private ProductRepository productRepository;
+
+  @Autowired
+  private StoreRepository storeRepository;
 
   @Autowired
   private StoreService storeService;
@@ -58,4 +68,25 @@ public class CreateStock {
 
     return createdInventory;
   }
+
+  @Transactional
+  public void generateStock() {
+    List<Product> products = productRepository.findAll();
+    List<Store> stores = storeRepository.findAll();
+    for (Product product : products) {
+      for (Store store : stores) {
+        if (!inventoryRepository.isExist(product.getId(), store.getId())) {
+          Inventory inventory = new Inventory();
+          inventory.setCode(sequenceService.generateUniqueCode("inventory_code_sequence", "IVT%05d"));
+          inventory.setProduct(product);
+          inventory.setStore(store);
+          Long stock = 0L;
+          inventory.setStock(stock); 
+
+          inventoryRepository.save(inventory);
+        }
+      }
+    }
+  }
+
 }
