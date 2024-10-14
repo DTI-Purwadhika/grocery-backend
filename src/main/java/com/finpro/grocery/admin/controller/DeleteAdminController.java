e87ea9c6c96c49497ddb5ac11272c0c4ed3d5c1c
Christofer Bryan <christoferbryan45@gmail.com>
2024-10-14 23:21:36 +0700

    reconfigured security config and admin controller method security

--- src/main/java/com/finpro/grocery/admin/controller/DeleteAdminController.java
+++ src/main/java/com/finpro/grocery/admin/controller/DeleteAdminController.java
@@ -4,6 +4,7 @@ import com.finpro.grocery.admin.dto.response.AdminResponseDTO;
 import com.finpro.grocery.admin.service.DeleteAdmin;
 import com.finpro.grocery.share.response.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,11 +19,13 @@ public class DeleteAdminController {
   private DeleteAdmin admin;
 
   @DeleteMapping("/{id}")
+  @PreAuthorize("hasAuthority('SCOPE_SUPER')")
   public ApiResponse<AdminResponseDTO> removeAdmin(@PathVariable Long id) {
     return new ApiResponse<>("DELETED", "200", admin.remove(id));
   }
 
   @PutMapping("/{id}/restore")
+  @PreAuthorize("hasAuthority('SCOPE_SUPER')")
   public ApiResponse<AdminResponseDTO> restoreAdmin(@PathVariable Long id) {
     return new ApiResponse<>("OK", "200", admin.restore(id));
   }
